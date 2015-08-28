a4404cab8247c268c27a0b50b9f1c1ed225b22e4
ruby -v ruby -v <ajhofferber@gmail.com>
2015-08-28 11:12:45 -0400

    adds seed data for items table

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+require "./models/item"
+
+Item.create(name: "Puffy Taco", category: "foods", sub_category: "Mexican", price: 4, description: "Refried beans, white cheddar, avocado, crema, pico de gallo, salsa verde. Add carnitas or asada")
+
+Item.create(name: "Daiquiri", category: "drinks", sub_category: "Tiki Drink", price: 8, description: "The Holy Trinity: lime, sugar, rum. Diluted to satisfy some asshole poet")
